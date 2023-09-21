3433a07a26aa3278b5dba38e1a58b6c12a1aa7e2
Cruz Wilfrido Ek Martin <gamecruz@gmail.com>
2023-09-20 23:01:18 -0500

    se agregan validaciones al formulario de crear vacante

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Salario;
 use Livewire\Component;
+use App\Models\Categoria;
 
 class CrearVacante extends Component
 {
+
+    //para validaciones
+    public $titulo, $salario, $categoria, $empresa, $ultimo_dia, $descripcion, $imagen;
+
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required',
+    ];
+
+    public function crearVacante(){
+       $datos = $this->validate();
+    }
+
     public function render()
     {
-        return view('livewire.crear-vacante');
+        $salarios = Salario::all();
+        $categorias = Categoria::all();
+        return view('livewire.crear-vacante', [
+            'salarios' => $salarios,
+            'categorias' => $categorias
+        ]);
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\HtmlString;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -32,9 +33,13 @@ class AuthServiceProvider extends ServiceProvider
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
                 ->subject('Verificar Cuenta')
+                ->greeting('Hola ' . $notifiable->name)
                 ->line('Tu cuenta ya está casi lista, solo debes presionar el enlace a continuación')
                 ->action('Confirmar Cuenta', $url)
-                ->line('Si no creaste esta cuenta, puedes ignorar este mensaje');
+                ->line('Si no creaste esta cuenta, puedes ignorar este mensaje')
+                ->salutation(new HtmlString(
+                    'Saludos '.'<br>' .'<strong>'. 'DevJobs' . '</strong>'
+                    ));
         });
     }
 }
